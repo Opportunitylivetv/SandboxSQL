b9cc41db04386d46449c613c0028318a9be6286e
Peter Cottle <pcottle@fb.com>
2015-12-26 18:27:09 -0800

    sickkkkk table working

--- src/js/views/QueryResultView.js
+++ src/js/views/QueryResultView.js
@@ -4,6 +4,7 @@ var {
   StyleSheet,
   Text,
   View,
+  ListView,
   TouchableHighlight,
   PropTypes,
   TextInput,
@@ -24,26 +25,11 @@ var QueryResultView = React.createClass({
       isLoading: true,
       error: null,
       rows: [],
+      firstRow: null,
     };
   },
 
-  componentDidMount: function() {
-    var rows = [];
-    this.props.database.executeSQL(
-      this.props.query,
-      this.props.params || [],
-      (row) => rows.push(row),
-      (error) => {
-        this.setState({
-          isLoading: false,
-          error,
-          rows,
-        });
-      },
-    );
-  },
-
-  render: function() {
+  render: function () {
     if (this.state.isLoading) {
       return (
         <View style={[styles.wrapper, styles.loading]}>
@@ -55,25 +41,121 @@ var QueryResultView = React.createClass({
       );
     }
 
+    if (this.state.error) {
+      return (
+        <Text>
+          {this.state.error.toString()}
+        </Text>
+      );
+    }
+
+    if (!this.state.firstRow) {
+      return (
+        <Text>
+          Sorry, your query returned no results.
+        </Text>
+      );
+    }
+
     return (
       <View style={styles.wrapper}>
-        <Text>
-          Num Rows:
-          {this.state.rows.length}
-          {this.state.rows.map(
-            row => JSON.stringify(row)
-          ).join(', ')}
-        </Text>
-        <Text>
-          {!this.state.error ? null : this.state.error.toString()}
-        </Text>
+        {this.renderFirstRow()}
+        <ListView
+          automaticallyAdjustContentInsets={false}
+          dataSource={this.state.dataSource}
+          renderRow={this._renderCol}
+        />
       </View>
     );
   },
 
+  renderFirstRow: function() {
+    var values = [];
+    Object.keys(this.state.firstRow).forEach(function(key) {
+      values.push(
+        <View style={styles.columnHeader} key={key}>
+          <Text>
+            {key}
+          </Text>
+        </View>
+      );
+    })
+    return (
+      <View style={styles.columnHeaderContainer}>
+        {values}
+      </View>
+    );
+  },
+
+  getInitialState: function() {
+    var dataSource = new ListView.DataSource(
+      {rowHasChanged: (r1, r2) => r1.name !== r2.name }
+    );
+    return { dataSource };
+  },
+
+  _renderCol: function(col) {
+    var values = [];
+    Object.keys(col).forEach(function(key) {
+      values.push(
+        <View style={styles.column} key={key}>
+          <Text>
+            {col[key]}
+          </Text>
+        </View>
+      );
+    })
+    return (
+      <View style={styles.colContainer}>
+        {values}
+      </View>
+    );
+  },
+
+  componentDidMount: function() {
+    var rows = [];
+    var firstRow = null;
+    this.props.database.executeSQL(
+      this.props.query,
+      this.props.params || [],
+      (row) => {
+        firstRow = firstRow || row;
+        rows.push(row);
+      },
+      (error) => {
+        this.setState({
+          isLoading: false,
+          firstRow,
+          error,
+          rows,
+          dataSource: this.state.dataSource.cloneWithRows(rows),
+        });
+      },
+    );
+  },
 });
 
 var styles = StyleSheet.create({
+  colContainer: {
+    flexDirection: 'row',
+    flex: 1,
+  },
+  columnHeaderContainer: {
+    flexDirection: 'row',
+  },
+  columnHeader: {
+    padding: 5,
+    backgroundColor: '#0066cc',
+    borderColor: 'grey',
+    borderWidth: 1,
+    flex: 1,
+  },
+  column: {
+    padding: 5,
+    borderColor: 'grey',
+    borderWidth: 1,
+    flex: 1,
+  },
   wrapper: {
     flex: 1,
   },
